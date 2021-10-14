18fbe50e0cf371ca5170028adc16ec251a6d1392
delaquash <olaidemmanuel1@gmail.com>
2021-10-14 17:00:08 +0100

    fixed getting specific route

--- routes/quotes.js
+++ routes/quotes.js
@@ -4,16 +4,24 @@ const Quotes = require('../Models/quotes');
 
 
 // get all routes
-router.get('/', (req, res) => {
-    res.send('Hello World')
+router.get('/', async (req, res) => {
+    const quote= await Quotes.find();
+    res.json(quote);
     
 });
 
+
+// post to all route
 router.post('/new', async (req, res) => {
     const newQuote = new Quotes(req.body);
     const savedQuote = await newQuote.save();
-    res.json(savedQuote);
-
+    res.json(newQuote);
 });
 
+
+// getting specific route
+router.get('/get/:id', async (req, res) => {
+    const q = await Quotes.findById({ _id: req.params.id });
+    res.json(q)
+})
 module.exports = router;
