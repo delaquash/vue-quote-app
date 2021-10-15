8b9d0faf08b2277620682d97d0c630b63839433d
delaquash <olaidemmanuel1@gmail.com>
2021-10-15 11:44:24 +0100

    fixed updating a single quote

--- routes/quotes.js
+++ routes/quotes.js
@@ -23,5 +23,20 @@ router.post('/new', async (req, res) => {
 router.get('/get/:id', async (req, res) => {
     const q = await Quotes.findById({ _id: req.params.id });
     res.json(q)
+});
+
+// delete quote
+router.delete('/delete/:id', async (req, res) => {
+    const result = await Quotes.findByIdAndDelete({_id: req.params.id});
+    res.json(result);
 })
+
+// update a quote
+router.patch('/update/:id',async  (req, res) => {
+    const updatedQuote = await Quote.updateOne({ _id: req.params.id}, {$set: req.body});
+    res.json(updatedQuote)
+});
+
+
+
 module.exports = router;
